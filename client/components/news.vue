6711d4053d006812ddc88be900369c91e3f0bb4c
polyester <56536708+polyester-CTRL@users.noreply.github.com>
2019-11-01 18:16:18 +0900

    枠の作成

--- client/components/news.vue
+++ client/components/news.vue
@@ -1,15 +1,43 @@
 <template>
   <div>
     <h1>News</h1>
-    <div>this is first news</div>
-    <div>this is scecond news</div>
+    <div>this is first news.</div>
+    <div class="flexNews">
+      <mainGuide name="first news" class="main guide" color="#4ff0f0" />
+    </div>
+    <div>this is second news.</div>
+    <div class="flexNews">
+    <mainGuide name="second news" class="main guide" color="#4f4fff" />
+    </div>
+    <!-- <div id="app1">{{ greeting }}</div> -->
   </div>
 </template>
 <script>
-export default {};
+//import news1 from './news1'
+import mainGuide from "~/components/news1";
+export default {
+  /*data () {
+    return {
+      // greeting: <mainGuide name="4" class="main guide" color="black" />
+      greeting:'third'
+    }
+  }*/
+  components: {
+    mainGuide
+  }
+};
 </script>
 <style lang="scss" scoped>
 div {
-  border: solid black 2px;
+  border: solid black 0px;
+}
+.flexNews {
+  width: 100%;
+  //display: flex;
+  border: solid black 1px;
+  justify-content: space-around;
+  flex-wrap: wrap;
+
+  /* width: 100;*/
 }
 </style>
