8e1fba04874e599027ca2dd3c7229f3c0b43e851
chikokuma <franker675@gmail.com>
2019-12-14 15:10:57 +0900

    ハンバーガータブのクリックイベントを追加

--- client/components/header.vue
+++ client/components/header.vue
@@ -7,22 +7,37 @@
         </nuxt-link>
       </div>
       <div class="theflex_cell">
-        <div class="second">
-          <span>～</span><span>C</span><span>T</span><span>R</span><span>L</span><span>ホ</span><span>ー</span><span>ム</span><span>ペ</span><span>ー</span><span>ジ</span><span>へ</span><span>よ</span><span>う</span><span>こ</span><span>そ</span><span>～</span>
-        </div>
-      </div>
-      <div class="theflex_cell">
         <form class="search_form">
           <input type="text" placeholder="検索内容を入力" class="textform"/><!--
           --><input type="submit" value="検索" class="button" />
         </form>
+      </div>
+      <div class="theflex_cell">
+        <div class="humburger" v-on:click='isOpen=!isOpen'>
+          <span class="top"></span>
+          <span class="middle"></span>
+          <span class="bottom"></span>
+        </div>
+      </div>
+      <div id="menu" class="menu" v-bind:class='{open:isOpen}'>
+        <ul>
+          <li>Top</li>
+          <li>活動報告</li>
+          <li>作品紹介</li>
+          <li>お問合せ</li>
+        </ul>
       </div>
     </header>
   </div>
 </template>
 <script>
 export default {
-  name: "theHeader"
+  name: "theHeader",
+  data() {
+    return{
+      isOpen: false
+    }
+  }
 };
 </script>
 <style lang="scss">
@@ -90,87 +105,51 @@ export default {
   }
 }
 
-//文字のcss
-.second{
-  display: none;
+
+.theflex_cell:last-child{
   @include mq{
-    display: block;
-    span{
-      color:#328d55;
-      font-size: 18px;
-      display: inline-block;
+    display: none;
+  }  
+}
+
+.humburger{
+  display: flex;
+  flex-direction: column;
+  height: 60px;
+  width: 60px;
+  padding: 4px;
+    & span{
+      height: 2px;
+      background-color: #c9c2c2;
+      margin: 7px;
     }
-    &:hover{
-      cursor: pointer;
+}
+
+.menu{
+  display: none;
+}
+
+.menu.open{
+  display: block;
+  z-index: 5;
+  width: 100%;
+  position: absolute;
+  padding: 10px;
+  top:60px;
+  right: 0;
+  background-color: #328d55;
+  line-height: 60px;
+  text-align: center;
+  & ul{
+    padding:0;
+    & li{
+    padding: 2px;
+    margin: 10px;
+    border: solid 2px #c9c2c2;
     }
   }
 }
-.second:hover span{
-  animation: hove 0.5s ease-in-out;
-}
 
-  @keyframes hove{
-    0%{
-      transform: translateY(0px);
-    }
-    33%{
-      transform: translateY(-10px);
-      color: #ff00b3;
-    }
-    100%{
-      transform: translateY(0px);
-    }
-  }
 
-.second:hover span:nth-child(1) {
-  animation-delay: 0;
-}
-.second:hover span:nth-child(2) {
-  animation-delay: 25ms;
-}
-.second:hover span:nth-child(3) {
-  animation-delay: 50ms;
-}
-.second:hover span:nth-child(4) {
-  animation-delay: 75ms;
-}
-.second:hover span:nth-child(5) {
-  animation-delay: 100ms;
-}
-.second:hover span:nth-child(6) {
-  animation-delay: 125ms;
-}
-.second:hover span:nth-child(7) {
-  animation-delay: 150ms;
-}
-.second:hover span:nth-child(8) {
-  animation-delay: 175ms;
-}
-.second:hover span:nth-child(9) {
-  animation-delay: 200ms;
-}
-.second:hover span:nth-child(10) {
-  animation-delay: 225ms;
-}
-.second:hover span:nth-child(11) {
-  animation-delay: 250ms;
-}
-.second:hover span:nth-child(12) {
-  animation-delay: 275ms;
-}
-.second:hover span:nth-child(13) {
-  animation-delay: 300ms;
-}
-.second:hover span:nth-child(14) {
-  animation-delay: 325ms;
-}
-.second:hover span:nth-child(15) {
-  animation-delay: 350ms;
-}
-.second:hover span:nth-child(16) {
-  animation-delay: 375ms;
-}
-.second:hover span:nth-child(17) {
-  animation-delay: 400ms;
-}
+
 </style>
