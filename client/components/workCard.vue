be166dc02921150cf3c445309386045d2dd1ed65
cotton <chinoknct@gmail.com>
2019-12-14 15:52:38 +0900

    詳細ページへのリンクを作成 一覧ページに表記する情報を整理

--- client/components/workCard.vue
+++ client/components/workCard.vue
@@ -2,27 +2,34 @@
     <div class="content">
         <el-card class="box-card">
             <div slot="header" class="clearfix">
-                <span>作品名</span>
+                <h3><nuxt-link to="/pastWorks/_pastWork">作品名</nuxt-link></h3>
             </div>
-            <div>
+            <div class="img_holder">
                 <img :src="img"/>
             </div>
-            <div v-for="o in 4" :key="o" class="text item">
-                {{'項目 ' + o }}
+            <!-- <div v-for="o in 4" :key="o" class="text item"> -->
+            <div class="text item">
+                {{ '日付 : ' + sample_data.date }}
+                <br>
+                {{ '概要 : ' + sample_data.overview }}
             </div>
         </el-card>
     </div>
 </template>
 
 <script>
-import twitter_img from "~/assets/img/img5.jpg"
+import work_img from "~/assets/img/img4.jpg"
 
 export default {
 
     //この辺全部progress.vueに移行
     data() {
         return {
-            img: twitter_img
+            img: work_img,
+            sample_data: {
+                date: '2019-12-13',
+                overview: 'この作品の概要です。',
+            }
         }
     },
     async asyncData({ app }) {
@@ -33,6 +40,10 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.content {
+    width: 100%;
+}
+
 .text {
 // font-size: 14px;
 }
@@ -55,12 +66,16 @@ clear: both
 }
 
 .box-card {
-width: 90%;
+width: 100%;
 background-color: #2c2c2c;
 color: $mainchar;
 }
 
+.img_holder {
+    width: 100%;
+}
+
 img {
-    width: 35px;
+    width: 80%;
 }
 </style>
--- client/pages/progress.vue
+++ client/pages/progress.vue
@@ -25,6 +25,11 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
+.content {
+  width: 100%;
+  padding: 0px 5%;
+}
+
 .el-col {
   margin: 20px 0px;
 }
