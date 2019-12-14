7fb9f4e1cc271d940012f20f3304f66c0789607b
polyester_ctrl <shunfreed2016@gmail.com>
2019-12-14 21:58:04 +0900

    メールアドレス入力欄を追加

--- client/pages/inquiry.vue
+++ client/pages/inquiry.vue
@@ -1,8 +1,41 @@
 <template>
-  <div>問い合わせ欄</div>
+  <div>
+    <div>
+      <h1>問い合わせ欄</h1>
+      問い合わせはこちらにお願いします。必要に応じて返信用のメールアドレスを入力してください。
+    </div>
+    <div class="control">
+      <!-- <h3>連絡先</h3> -->
+      <p style="font-size: 14px">次のフォームに必要事項をご記入ください。</p>
+      <label for="mymail">
+        メールアドレス：
+        <span class="required">任意</span>
+      </label>
+      <input id="mymail" type="email" name="mymail" value="me@example.com" />
+    </div>
+    <div class="control">
+      <button type="submit">送信</button>
+    </div>
+  </div>
 </template>
 <script>
 export default {};
 </script>
 <style lang="scss" scoped>
+.control {
+  margin: 2em;
+}
+label {
+  display: block;
+  margin-bottom: 0.5em;
+}
+
+.required {
+  margin-left: 0.3em; /*左の余白を0.3em(1文字の30%)に*/
+  color: #f33;
+  font-size: 0.9em;
+  padding: 3px; /*要素内余白を3pxに*/
+  background-color: #fee;
+  font-weight: bold;
+}
 </style>
