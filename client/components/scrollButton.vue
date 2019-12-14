1cbce810be234abdfde585934e2a8a82aa99e98b
chikokuma <franker675@gmail.com>
2019-12-15 01:10:41 +0900

    スクロールボタンをimport

---
+++ client/components/scrollButton.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <div class="button">
+            top
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: "scrollButton"
+}
+</script>
+
+<style lang="scss" scoped>
+    .button{
+        width: 100px;
+        height: 100px;
+        border: solid 2px;
+        position: fixed;
+        z-index: 99;
+        background-color: pink;
+        top: 0;
+        right: 0;
+    }
+</style>
