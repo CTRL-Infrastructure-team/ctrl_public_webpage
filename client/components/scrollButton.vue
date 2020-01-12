6116d28229330fc923c64223ae7e4f0ba66b0b1b
chikokuma <franker675@gmail.com>
2020-01-12 12:36:49 +0900

    css

--- client/components/scrollButton.vue
+++ client/components/scrollButton.vue
@@ -1,25 +1,67 @@
 <template>
     <div>
-        <div class="button">
-            top
+        <div class="button" @click="gotop">
+            
+            
+            <span>
+                <div class="left">
+                </div>
+                <div class="right">
+                </div>
+            </span>
+            
+            
         </div>
     </div>
 </template>
 <script>
 export default {
-    name: "scrollButton"
+    name: "scrollButton",
+    methods: {
+        gotop: function() {
+           window.scrollTo({
+                top: 0,
+                behavior: "smooth"
+            });
+        }
+    }
 }
 </script>
 
 <style lang="scss" scoped>
     .button{
-        width: 100px;
-        height: 100px;
-        border: solid 2px;
+        width: 50px;
+        height: 50px;
+        border-radius: 50%;
         position: fixed;
         z-index: 99;
-        background-color: pink;
-        top: 0;
-        right: 0;
+        background-color: #328d55;
+        bottom: 5%;
+        right: 5%;
     }
+
+    span{
+        z-index: 101;
+        width: 100%;
+        height: 100%;
+        display: block;
+        position: relative;
+    }
+
+    .left{
+        position: absolute;
+        width: 50%;
+        height: 5px;
+        background-color: #c9c2c2;
+        transform: translateX(20%) translateY(400%) rotate(-45deg);
+    }
+
+    .right{
+        position: absolute;
+        width: 50%;
+        height: 5px;
+        background-color: #c9c2c2;
+        transform: translateX(80%) translateY(400%) rotate(45deg);
+    }
+
 </style>
