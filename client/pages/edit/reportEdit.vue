9f60435d7a5bcffb99d735ca46c411a7139e0468
polyester_ctrl <shunfreed2016@gmail.com>
2020-03-12 03:17:25 +0900

    活動報告用の編集ページを仮作成

---
+++ client/pages/edit/reportEdit.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <div class="page-title">
+      活動報告の編集ページ
+    </div>
+    <div class="form">
+      <div>
+        タイトル:
+      </div>
+      <div>
+        内容:
+      </div>
+      <div>
+        画像:
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style lang="scss" scoped>
+.page-title {
+  height: 30px;
+  margin-top: 20px;
+  margin-bottom: 10px;
+  margin-left: 30px;
+  font-size: calc(10px + 0.800vw);
+}
+.form {
+  height: 300px;
+  margin-top: 10px;
+  margin-bottom: 20px;
+  margin-left: 30px;
+}
+
+  
+</style>
