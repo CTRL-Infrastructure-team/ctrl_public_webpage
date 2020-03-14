86cee8f6af1a62ca26fbd195e2e3dff8129b2193
chikokuma <franker675@gmail.com>
2020-03-14 22:49:50 +0900

    画像二枚目の選択

--- client/pages/edit/pushWorks.vue
+++ client/pages/edit/pushWorks.vue
@@ -3,7 +3,8 @@
         <form class="inputform">
             title<br><input type="text" placeholder="タイトルを入力"/><br>
             content<br><textarea rows="7" cols="100">内容を入力</textarea><br>
-            image<br><input type="file" accept="image/*"/><br>
+            image1<br><input type="file" accept="image/*"/><br>
+            image2<br><input type="file" accept="image/*"/><br>
             file<br><input type="file"/><br>
             <input type="submit">
         </form>
