acc71a1945c5aa348de170a16dd00f3f101dcc28
chikokuma <franker675@gmail.com>
2020-03-14 22:43:10 +0900

    最低限の入力フォームを設定

--- client/pages/edit/pushWorks.vue
+++ client/pages/edit/pushWorks.vue
@@ -1,5 +1,12 @@
 <template>
     <div>
+        <form class="inputform">
+            title<br><input type="text" placeholder="タイトルを入力"/><br>
+            content<br><textarea rows="7" cols="100">内容を入力</textarea><br>
+            image<br><input type="file" accept="image/*"/><br>
+            file<br><input type="file"/><br>
+            <input type="submit">
+        </form>
     </div>
 </template>
 <script>
@@ -8,5 +15,7 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
-
+.inputform{
+    margin: 10px;
+}
 </style>
