f3e378aea3bae6fa1433be3a65219feb2085af52
chikokuma <franker675@gmail.com>
2020-03-15 22:55:13 +0900

    ドラッグアンドドロップの仮実装

--- client/pages/edit/pushWorks.vue
+++ client/pages/edit/pushWorks.vue
@@ -3,7 +3,11 @@
         <form class="inputform">
             title<br><input type="text" placeholder="タイトルを入力"/><br>
             content<br><textarea rows="7" cols="100">内容を入力</textarea><br>
-            image1<br><input type="file" accept="image/*"/><br>
+            <div class="drop" v-bind:class="{'changed':changed}" @dragover.prevent="makeborder($event)" @drop.prevent="upload($event)">
+            image1<br><input type="file" id="upload_image" accept="image/*"/><br>
+            <p >ここに画像をドロップ</p>
+            <img v-show="preview" v-bind:src="preview" style="width:300px;">
+            </div>
             image2<br><input type="file" accept="image/*"/><br>
             file<br><input type="file"/><br>
             <input type="submit">
@@ -12,11 +16,36 @@
 </template>
 <script>
 export default {
-    
+    data:{
+        preview:'',
+        changed:false,
+    },
+    methods:{
+        upload: function(event){
+            const files = event.target.files ? event.target.files : event.dataTransfer.files;
+            const file = files[0];
+            const reader = new FileReader();
+            reader.onload = event => {
+                this.preview = event.target.result;
+            };
+            reader.readAsDataURL(file);
+            this.name = files[0].name;
+            document.getElementById("upload_image").files = files;
+        },
+        makeborder: function(event){
+            this.changed = true;
+        }
+    }
 }
 </script>
 <style lang="scss" scoped>
 .inputform{
     margin: 10px;
+}
+.drop{
+    border: 1px solid black;
+}
+.changed{
+    border: 3px solid green;
 }
 </style>
