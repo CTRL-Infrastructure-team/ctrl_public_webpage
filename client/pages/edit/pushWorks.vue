57b7fd2b8b8c9a6d805f9bdde82ee30bb9b1639f
chikokuma <franker675@gmail.com>
2020-03-26 21:42:21 +0900

    形だけ

--- client/pages/edit/pushWorks.vue
+++ client/pages/edit/pushWorks.vue
@@ -21,26 +21,53 @@
           <p>成果物の内容を入力してください</p>
           <el-input
             placeholder="内容を入力"
-            v-model="inquiry.value"
+            v-model="content.value"
             name="content"
             type="textarea"
             rows="7"
             cols="100"
             @change="doValidateInquiry(content)"
           ></el-input>
-          {{inquiry.alert}}
+          {{content.alert}}
         </div>
         <div class="images">
           <div class="image1">
-            画像1:
+            <div>
+            画像1：
+            </div>
             <el-upload
               class="upload-demo"
               drag
               action="localhost:3000"
-              :on-preview="handlePreview"
-              :on-remove="handleRemove"
+              :on-preview="handlePreview1"
+              :on-remove="handleRemove1"
               :file-list="fileList1"
               :auto-upload="false"
+              :list-type="picture"
+              :limit="1"
+              @change="setImage1($event)"
+              multiple>
+              <i class="el-icon-upload"></i>
+              <div class="el-upload__text">ここにファイルをドロップ <br><em>またはクリックしてアップロード</em></div>
+              <div class="el-upload__tip" slot="tip">jpg/png files with a size less than 500kb</div>
+            </el-upload>
+            <p>
+              <img :src="data.image1">
+            </p>
+          </div>
+          <div class="image2">
+            <div>
+            画像2：
+            </div>
+            <el-upload
+              class="upload-demo2"
+              drag
+              action="localhost:3000"
+              :on-preview="handlePreview2"
+              :on-remove="handleRemove2"
+              :file-list="fileList2"
+              :auto-upload="false"
+              :list-type="picture"
               :limit="1"
               multiple>
               <i class="el-icon-upload"></i>
@@ -48,25 +75,8 @@
               <div class="el-upload__tip" slot="tip">jpg/png files with a size less than 500kb</div>
             </el-upload>
             <p>
-              <img src="data.image1">
+              <img :src="data.image2">
             </p>
-          </div>
-          <div class="image2">
-            画像2：
-            <el-upload
-              class="upload-demo2"
-              drag
-              action="localhost:3000"
-              :on-preview="handlePreview"
-              :on-remove="handleRemove"
-              :file-list="fileList2"
-              :auto-upload="false"
-              :limit="1"
-              multiple>
-              <i class="el-icon-upload"></i>
-              <div class="el-upload__text">ここにファイルをドロップ <br><em>またはクリックしてアップロード</em></div>
-              <div class="el-upload__tip" slot="tip">jpg/png files with a size less than 500kb</div>
-            </el-upload>
           </div>
         </div>
           <div fileUpload>
@@ -77,7 +87,7 @@
                 action="localhost:3000"
                 :on-preview="handlePreview"
                 :on-remove="handleRemove"
-                :file-list="fileList3"
+                :file-list="fileList"
                 :auto-upload="false"
                 :limit="1"
                 multiple>
@@ -98,12 +108,11 @@
 <script>
 import { Input } from 'element-ui'
 export default {
-     data(){
-    return{
+     data() {
+       return{
       data:{
         image1:'',
         image2:'',
-
       },
       title:{
         value:'',
@@ -115,7 +124,7 @@ export default {
       },
       fileList1:[],
       fileList2:[],
-      fileList2:[],
+      fileList:[],
       checked: false,
       alert:''
     }
@@ -125,6 +134,18 @@ export default {
       console.log(file, fileList);
     },
     handlePreview(file){
+      console.log(file);
+    },
+    handleRemove1(file,fileList1){
+      console.log(file, fileList);
+    },
+    handlePreview1(file){
+      console.log(file);
+    },
+    handleRemove2(file,fileList2){
+      console.log(file, fileList);
+    },
+    handlePreview2(file){
       console.log(file);
     },
     doSendFile(){
@@ -138,6 +159,7 @@ export default {
     doValidateInquiry(data,index){
       this.content.value ? '': this.content.alert = '値を入力してください'
    },
+   
   }
 }
 </script>
@@ -156,15 +178,22 @@ span{
 .images{
     
     display:flex;
-    flex-direction: rows;
-    justify-content: space-around;
+    flex-direction: column;
     padding: 20px;
 }
 .image1{
     margin: 10px;
+    display: flex;
+    flex-direction: row;
+    & p{
+      width: 100px;
+      height: 100px;
+    }
 }
 .image2{
     margin: 10px;
+    display: flex;
+    flex-direction: row;
 }
 }
 </style>
