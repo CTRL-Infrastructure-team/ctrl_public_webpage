36e8c5f613a5bfe623a86acaf2850ee28aa54d3a
cotton <chinoknct@gmail.com>
2020-04-02 00:33:33 +0900

    [fix]ユーザ作成処理の修正
    ユーザ作成処理に若干の修正を加えました。

--- api/test/create.js
+++ api/test/create.js
@@ -1,5 +1,27 @@
+const mongoose = require("mongoose");
+var os = require("os");
+var hostname = os.hostname();
+const tl =
+  hostname.includes("DESKTOP") ||
+  hostname.includes("localhost") ||
+  hostname.includes("MAC");
+  
 const userData = require('./user_data.json'),
-      User = require("../medels/user");
+      User = require("../models/user");
+
+      if (tl) {
+  mongoose.connect("mongodb://localhost:27017/ctrlPublicSite", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+} else {
+  mongoose.connect("mongodb://localhost:27017/ctrlPublicSite", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+}
+mongoose.Promise = global.Promise;
+
 
 for(let slot of userData) {
   User.create(slot);
