d9efaba81a89b8b95fe7b6c59840171758b66c52
polyester_ctrl <shunfreed2016@gmail.com>
2021-11-25 02:09:24 +0900

    ファイル書き込み処理の実装

--- api/config/localfile.js
+++ api/config/localfile.js
@@ -1,7 +1,33 @@
+var fs = require('fs');
+const requestJudge = (requestPath) => {
+  if(requestPath === 'situation') {
+    return 'ctrl-situations';
+  } else {
+    return 'ctrl-pastworks';
+  }
+};
+
+const fileNameModify = (requestPath, filePath) => {
+  if(requestPath === 'situation') {
+    return filePath.substring(93);
+  } else {
+    return filePath.substring(92);
+  }
+};
+
 module.exports = {
   uploadFiles(requestPath, fileList, workIdentification) {
-    
+    let containerName = requestJudge(requestPath);
+    fileList.map((file) => {
+      fs.writeFile('./data/' + containerName + '/' + workIdentification + file.originalname, file, function (err) {
+        if (err) {
+          throw err;
+        }
+      });
+    })
   },
   deleteFiles(requestPath, filePath) {
+    let containerName = requestJudge(requestPath),
+        fileName = fileNameModify(requestPath, filePath);
   }
 }
