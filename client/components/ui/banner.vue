ef1bf12dc05207cde0a45208f95060758778401c
DESKTOP-ARL3CET\zawa_ <hanazawa9to13ctrl@gmail.com>
2022-03-17 22:44:47 +0900

    第3回進捗発表スライドの公開

---
+++ client/components/ui/banner.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="banner-wrapper">
+    <nuxt-link class="banner-link" to="/special/announcementList"></nuxt-link>
+    <span class="banner-top">進捗発表資料 公開中</span>
+    <span class="banner-description">詳しくはこちらをクリック！</span>
+  </div>
+  <!-- /.banner-wrapper -->
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+.banner {
+  &-wrapper {
+    position: relative;
+    margin: 0 auto;
+    margin-top: 30px;
+    width: 90%;
+    height: 150px;
+    background: #f5f4f4;
+    border-radius: 3px;
+    text-align: center;
+    overflow: hidden;
+  }
+
+  &-top {
+    display: inline-block;
+    width: 100%;
+    font-size: calc(16px + 1vw);
+    font-weight: bolder;
+    line-height: 100px;
+    color: #000000;
+  }
+
+  &-description {
+    display: inline-block;
+    width: 100%;
+    font-size: calc(15px + 0.7vw);
+    font-weight: bolder;
+    line-height: 40px;
+    color: #000000;
+  }
+
+  &-link {
+    position: absolute;
+    display: block;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+  }
+}
+
+</style>
---
+++ client/pages/special/announcementList.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="container">
+    <div class="breadcrumb-wrapper">
+      <ul class="breadcrumb-list">
+        <li class="breadcrumb-path" v-for="data in path_datas" :key="data.id">
+          <nuxt-link class="breadcrumb-link" :to="data.path">{{
+            data.name
+          }}</nuxt-link>
+        </li>
+      </ul>
+    </div>
+    <!-- /.breadcrumb-wrapper -->
+    <div class="list-wrapper">
+      <!-- <bannerTimes :times="times.first" /> --> <!--非公開のためコメント化-->
+      <!-- <bannerTimes :times="times.second" /> --> <!--非公開のためコメント化-->
+      <bannerTimes :times="times.third" />
+    </div>
+    <!-- /.list-wrapper -->
+  </div>
+  <!-- /.container -->
+</template>
+
+<script>
+import bannerTimes from "../../components/special/bannerTimes";
+
+export default {
+  components: { bannerTimes },
+  data() {
+    return {
+      path_datas: [
+        { name: "Top", path: "/" },
+        { name: "特設ページTop", path: "/special/announcementList" }
+      ],
+      times: {
+        first: { top: "第1回進捗発表資料", link: "/special/firstPresentations" },
+        second: { top: "第2回進捗発表資料", link: "/special/secondPresentations" },
+        third: { top: "第3回進捗発表資料", link: "/special/thirdPresentations" },
+      }
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.container {
+  width: 100%;
+  margin: 30px auto;
+  @include mq {
+    margin: 50px auto;
+  }
+}
+
+.breadcrumb {
+  &-wrapper {
+    width: 90%;
+    max-width: 1000px;
+    margin: 0px auto 30px auto;
+  }
+  &-list {
+    padding-left: 0px;
+    text-align: left;
+  }
+  &-path {
+    display: inline-block;
+    list-style: none;
+  }
+  &-path::after {
+    display: inline-block;
+    margin: 0px 5px;
+    content: ">";
+  }
+  &-path:last-child::after {
+    content: none;
+  }
+  &-link {
+    text-decoration: none;
+    color: $mainchar;
+  }
+  &-text {
+    margin-top: 20px;
+  }
+}
+
+.list-wrapper {
+  min-height: calc(100vh - 60px);
+  width: 90%;
+  max-width: 1000px;
+  margin: 30px auto;
+}
+
+</style>
