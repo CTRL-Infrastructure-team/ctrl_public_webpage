7704c50c9f84bcaf42b06a93f26cbdadad0dd74a
DESKTOP-823UP9R\zawa <hanazawa9to13ctrl@gmail.com>
2022-07-10 02:20:37 +0900

    2022_07_06_第０回進捗発表会のスライド公開

--- client/pages/special/announcementList.vue
+++ client/pages/special/announcementList.vue
@@ -11,11 +11,13 @@
     </div>
     <!-- /.breadcrumb-wrapper -->
     <div class="list-wrapper">
+      <bannerTimes :times="times.zeroth" />
       <!-- <bannerTimes :times="times.first" /> --> <!--非公開のためコメント化-->
       <!-- <bannerTimes :times="times.second" /> --> <!--非公開のためコメント化-->
-      <bannerTimes :times="times.third" />
+      <!--<bannerTimes :times="times.third" />--> <!--非公開のためコメント化-->
+      <!--<bannerTimes :times="times.final" />--> <!--非公開のためコメント化-->
       <bannerTimes :times="times.osigengo" />
-      <bannerTimes :times="times.final" />
+      
     </div>
     <!-- /.list-wrapper -->
   </div>
@@ -45,11 +47,12 @@ export default {
         { name: "特設ページTop", path: "/special/announcementList" }
       ],
       times: {
+        zeroth: { top: "第0回進捗発表", link: "/special/zerothPresentations" },
         //first: { top: "第1回進捗発表", link: "/special/firstPresentations" },
         //second: { top: "第2回進捗発表", link: "/special/secondPresentations" },
-        third: { top: "第3回進捗発表", link: "/special/thirdPresentations" },
+        //third: { top: "第3回進捗発表", link: "/special/thirdPresentations" },
+        //final: { top: "最終進捗発表会", link: "/special/finalPresentations" },
         osigengo: { top: "推し言語・ツール好き勝手喋り会", link: "/special/osigengoPresentations" },
-        final: { top: "最終進捗発表会", link: "/special/finalPresentations" },
       }
     };
   }
