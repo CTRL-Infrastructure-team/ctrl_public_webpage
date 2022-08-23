7d029cc88acbc2b277bca8b7649f37876497489b
DESKTOP-823UP9R\zawa <hanazawa9to13ctrl@gmail.com>
2022-08-23 20:48:53 +0900

    作品編集ページの仕様を作品投稿ページにそろえた

--- client/pages/edit/works/_updateWork.vue
+++ client/pages/edit/works/_updateWork.vue
@@ -8,84 +8,91 @@
       <div class="attention">
         そのため、更新する際は必ず画像及びゲームファイルを入力し直してください。
       </div>
-      <div class="form-inquiry form-box">
+      <div class="form-box">
         <p>
           <label for="content">タイトル</label>
           <span>(必須)</span>
         </p>
-        <el-input 
+        <div class="alert">
+          {{ title.alert }}
+        </div>
+        <el-input
           v-model="title.value"
           placeholder="タイトルを入力"></el-input>
       </div>
 
-      <div class="form-inquiry form-box">
+      <div class="form-box">
         <p>
           <label for="content">本文</label>
           <span>(必須)</span>
         </p>
-
-
-
+        <div class="alert">
+          {{ content.alert }}
+        </div>
         <el-input
-          placeholder="内容を入力"
           v-model="content.value"
+          placeholder="内容を入力"
           name="content"
           type="textarea"
           rows="7"
           cols="100"
-          @change="doValidateInquiry(content)"
         ></el-input>
       </div>
-      <div class="images">
-        <div class="image1">
-          <p>
-            <label for="content">トップ画像</label>
-            <span>(1枚必須)</span>
-          </p>
-          <el-upload
-            class="upload-demo"
-            drag
-            action=""
-            list-type="picture"
-            :on-change="changeTopImage"
-            :file-list="topImage"
-            :auto-upload="false"
-            :limit="1"
-            @change="setImage1($event)"
-            multiple>
-            <i class="el-icon-upload"></i>
-            <div class="el-upload__text">ここにファイルをドロップ <br><em>またはクリックしてアップロード</em></div>
-            <div class="el-upload__tip" slot="tip">jpg/png files with a size less than 500kb</div>
-          </el-upload>
-
+      <div class="form-box">
+        <p>
+          <label for="content">トップ画像</label>
+          <span>(1枚必須)</span>
+        </p>
+        <div class="alert">
+          {{ topImageAlert }}
         </div>
-        <div class="image2">
-          <p>
-            <label for="content">その他の画像</label>
-            <span>(2枚必須)</span>
-          </p>
-          <el-upload
-            class="upload-demo2"
-            drag
-            action=""
-            :on-change="changeOtherImage"
-            :file-list="otherImage"
-            :auto-upload="false"
-            list-type="picture"
-            :limit="2"
-            multiple>
-            <i class="el-icon-upload"></i>
-            <div class="el-upload__text">ここにファイルをドロップ <br><em>またはクリックしてアップロード</em></div>
-            <div class="el-upload__tip" slot="tip">jpg/png files with a size less than 500kb</div>
-          </el-upload>
-          <p><img :src="data.image2"></p>
-        </div>
+        <el-upload
+          class="upload-demo"
+          drag
+          action=""
+          list-type="picture"
+          :on-change="changeTopImage"
+          :file-list="topImage"
+          :auto-upload="false"
+          :limit="1"
+          @change="setImage1($event)"
+          multiple>
+          <i class="el-icon-upload"></i>
+          <div class="el-upload__text">ここにファイルをドロップ <br><em>またはクリックしてアップロード</em></div>
+          <div class="el-upload__tip" slot="tip">jpg/png files with a size less than 500kb</div>
+        </el-upload>
       </div>
-      <div class="file-input">
+      <div class="form-box">
+        <p>
+          <label for="content">その他の画像</label>
+          <span>(2枚必須)</span>
+        </p>
+        <div class="alert">
+          {{ otherImageAlert }}
+        </div>
+        <el-upload
+          class="upload-demo2"
+          drag
+          action=""
+          :on-change="changeOtherImage"
+          :file-list="otherImage"
+          :auto-upload="false"
+          list-type="picture"
+          :limit="2"
+          multiple>
+          <i class="el-icon-upload"></i>
+          <div class="el-upload__text">ここにファイルをドロップ <br><em>またはクリックしてアップロード</em></div>
+          <div class="el-upload__tip" slot="tip">jpg/png files with a size less than 500kb</div>
+        </el-upload>
+      </div>
+      <div class="form-box">
         <p>
           <label for="content">ファイルをアップロード</label>
           <span>(必須, zipファイル形式)</span>
         </p>
+        <div class="alert">
+          {{ gameFileAlert }}
+        </div>
         <el-upload
           class="upload-demo"
           drag
@@ -100,7 +107,7 @@
           <div class="el-upload__text">ここにファイルをドロップ <br><em>またはクリックしてアップロード</em></div>
           <div class="el-upload__tip" slot="tip">zip files with a size less than 100Mb</div>
         </el-upload>
-        
+
       </div>
       <div class="form-button">
         Twitter IDを掲載する
@@ -128,12 +135,14 @@ export default {
   },
   data() {
     return{
-      data: { image1:'', image2:'' },
       title: { value: '', alert:'' },
       content: { value:'', alert:'' },
       topImage: [],
       otherImage: [],
       gameFile: [],
+      topImageAlert: '',
+      otherImageAlert: '',
+      gameFileAlert: '',
       checked: false,
       alert: '',
       auth: false
@@ -154,40 +163,64 @@ export default {
   methods:{
     changeTopImage(file, topImage) {
       this.topImage = topImage;
-      console.log(this.title.value);
     },
     changeOtherImage(file, otherImage) {
       this.otherImage = otherImage;
-      console.log(this.otherImage);
     },
     changeGameFile(file, gameFile) {
       this.gameFile = gameFile;
-      console.log(this.gameFile);
     },
     doSendForm(){
-      let formData = new FormData(),
-          uploadFile = '',
-          uploadTopImage = '',
-          uploadContent = this.content.value.replace(/\n/g,"<br>");
+      let formData = new FormData();
 
-      if(this.gameFile[0] !== 'undefined') {
-        uploadFile = this.gameFile[0].raw;
+      if (this.title.value === "") {
+        this.title.alert = "タイトルを入力してください";
+      } else {
+        this.title.alert = "";
+        formData.append("title", this.title.value);
       }
-      if(this.topImage[0] !== 'undefined') {
-        uploadTopImage = this.topImage[0].raw;
+      
+      if (this.content.value === "") {
+        this.content.alert = "本文を入力してください";
+      } else {
+        this.content.alert = "";
+        formData.append("content", this.content.value.replace(/\n/g,"<br>"));
       }
 
-      if(this.otherImage !== 'undefined') {
+      if (this.topImage.length == 0) {
+        this.topImageAlert = "画像を1枚指定してください";
+      } else {
+        this.topImageAlert = "";
+        let uploadTopImage = this.topImage[0].raw;
+        formData.append('topImage', uploadTopImage);
+      }
+
+      if (this.otherImage.length < 2) {
+        this.otherImageAlert = "画像を2枚指定してください";
+      } else {
+        this.otherImageAlert = "";
         this.otherImage.map(image => {
-          formData.append('otherImage', image.raw);
-        });
+        formData.append('otherImage', image.raw);
+      })
       }
 
-      formData.append('gameFile', uploadFile);
-      formData.append('topImage', uploadTopImage);
-      formData.append('title', this.title.value);
-      formData.append('content', uploadContent);
+      if (this.gameFile.length == 0) {
+        this.gameFileAlert = "zipファイルを1つ指定してください";
+      } else {
+        this.gameFileAlert = "";
+        let uploadFile = this.gameFile[0].raw;
+        formData.append('gameFile', uploadFile);
+      }
+
       formData.append('twitter', this.checked);
+
+      if (this.title.alert != "" ||
+          this.content.alert != "" ||
+          this.topImageAlert != "" ||
+          this.otherImageAlert != "" ||
+          this.gameFileAlert != "") {
+        return;
+      }
 
       axios.put(`/api/pastWork/${this.$route.params.updateWork}`,
         formData,
@@ -197,33 +230,20 @@ export default {
         console.log(result);
       })
     },
-    doValidateTitle(data,index){
-      this.title.value ? '': this.title.alert = '値を入力してください'
-    },
-    doValidateInquiry(data,index){
-      this.content.value ? '': this.content.alert = '値を入力してください'
-    }
   }
 }
 </script>
 <style lang="scss" scoped>
-.el-upload__tip{
-  color: white;
-}
 
-.checkbox{
-  margin-top: 1.5em;
+.el-upload__tip {
   color: white;
-}
-
-.attention {
-  margin: 10px 0px;
-  font-size: calc(15px + 0.2vw);
 }
 
 .form {
   width: 90%;
   margin: 0 auto;
+  margin-top: 10px;
+  margin-bottom: 50px;
   &-box {
     margin-bottom: 1.8em;
     p:nth-child(1) {
@@ -233,9 +253,9 @@ export default {
       margin-bottom: 0.4rem;
     }
   }
-  &-button{
-    margin-top: 2.5em;
-    &-file{
+  &-button {
+    margin-top: 1em;
+    &-file {
       width: 10em;
       margin-bottom: 1em;
     }
@@ -252,5 +272,12 @@ export default {
     }
   }
 }
-
+.alert {
+  margin-top: 0.3rem;
+  margin-bottom: 0.3rem;
+  color: rgb(230, 30, 30);
+}
+.attention {
+  padding: 10px;
+}
 </style>
