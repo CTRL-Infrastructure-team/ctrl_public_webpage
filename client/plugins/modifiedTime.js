074cd76032870a31ccfd8eabef26baa3eb4e6a05
DESKTOP-823UP9R\zawa <hanazawa9to13ctrl@gmail.com>
2022-09-16 20:22:33 +0900

    進捗ページの日付のずれを修正。UTCから日本時間へ

--- client/plugins/modifiedTime.js
+++ client/plugins/modifiedTime.js
@@ -2,5 +2,5 @@ import moment from 'moment'
 
 export default (time) => {
     // return moment(time).add(9, 'h').format('YYYY年MM月DD日')
-    return moment(time).add(9, 'h').format('YYYY年MM月DD日')
+    return moment(time).local().format('YYYY年MM月DD日')
 }
